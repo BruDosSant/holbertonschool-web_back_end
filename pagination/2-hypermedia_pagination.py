a61c0bd380a15bd11712fdd75b47cd4cef482a1a
BruDosSant <brudossant@gmail.com>
2025-08-17 23:43:31 -0300

    anashe

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -70,17 +70,17 @@ class Server:
     
         """
         Return a dictionary with pagination details.
-       """
+        """
 
-    data = self.get_page(page, page_size)
-    total_items = len(self.dataset())
-    total_pages = math.ceil(total_items / page_size)
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
 
-    return {
-        'page_size': len(data),       # puede ser menor en la última página
+        return {
+        'page_size': len(data),
         'page': page,
         'data': data,
         'next_page': page + 1 if page < total_pages else None,
         'prev_page': page - 1 if page > 1 else None,
         'total_pages': total_pages
-    }
+        }
