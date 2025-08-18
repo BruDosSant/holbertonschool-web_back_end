789ca9aa9793fdac07b23e9e929b7f168437aa1d
BruDosSant <brudossant@gmail.com>
2025-08-17 23:51:11 -0300

    anashe

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,14 +1,68 @@
 #!/usr/bin/env python3
-
-
 """
-Ejercicio 1 clase para paginar una base de datos
+Deletion-resilient hypermedia pagination
 """
-
 
 import csv
 import math
 from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+
+        """
+        Deletion-resilient hypermedia pagination.
+        """
+
+        assert type(index) is int and index >= 0
+        assert type(page_size) is int and page_size > 0
+        assert index < len(self.__indexed_dataset)
+
+        data = []
+        current_index = index
+
+        while len(data) < page_size and current_index < len(self.__indexed_dataset):
+            if current_index in self.__indexed_dataset:
+                data.append(self.__indexed_dataset[current_index])
+            current_index += 1
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": current_index
+        }
 
 
 """
@@ -83,29 +137,4 @@ class Server:
             'next_page': page + 1 if page < total_pages else None,
             'prev_page': page - 1 if page > 1 else None,
             'total_pages': total_pages
-        }
-
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
-
-        """
-        Deletion-resilient hypermedia pagination.
-        """
-
-        assert type(index) is int and index >= 0
-        assert type(page_size) is int and page_size > 0
-        assert index < len(self.__indexed_dataset)
-
-        data = []
-        current_index = index
-
-        while len(data) < page_size and current_index < len(self.__indexed_dataset):
-            if current_index in self.__indexed_dataset:
-                data.append(self.__indexed_dataset[current_index])
-            current_index += 1
-
-        return {
-            "index": index,
-            "data": data,
-            "page_size": len(data),
-            "next_index": current_index
         }
