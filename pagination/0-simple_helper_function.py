95b0ac3d84607ce191e6061e11b5bb8dd4df99a8
BruDosSant <brudossant@gmail.com>
2025-08-17 22:29:23 -0300

    anashe

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,7 +9,7 @@ Como organizar en paginas
 def index_range(page: int, page_size: int) -> tuple:
 
     """retorna tupla de inicio y fin"""
-    
+
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
