14cb5183864044bb389bf3ae3388ad91501d0f56
BruDosSant <brudossant@gmail.com>
2025-08-20 02:42:43 +0000

    macaquinho

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+modulo 8-all
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    lista de escuelas por topic
+    """
+
+    lista = mongo_collection.find({topics: topic})
+    return list(lista)
