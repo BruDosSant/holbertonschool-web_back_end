c50d8aa23604389cecdf7c2d2f236edae45978a2
BruDosSant <brudossant@gmail.com>
2025-08-20 02:52:45 +0000

    macaquinho

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+
+"""
+modulo 8-all
+"""
+
+
+from pymongo import MongoClient
+if __name__ == "__main__":
+    client = MongoClient()
+
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    print(f"\tmethod GET: {collection.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection.count_documents({'method': 'DELETE'})}")
+
+    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
