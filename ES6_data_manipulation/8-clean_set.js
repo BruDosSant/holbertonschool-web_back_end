daf4631592a41a9851bff4d30e89620f7319b657
BruDosSant <brudossant@gmail.com>
2025-09-01 02:13:23 +0000

    messi?

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,5 @@
 export default function cleanSet(set, startString) {
+  if (!startString) return ''; // Maneja undefined o ''
   return [...set]
     .filter(val => val.startsWith(startString))
     .map(val => val.slice(startString.length))
