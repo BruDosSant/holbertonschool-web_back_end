a5e8dbbf086d9a1972a2d8db510bd78b3d80a936
BruDosSant <brudossant@gmail.com>
2025-09-07 14:40:53 -0300

    messi

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,40 @@
+// 2-read_file.js
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+  try {
+    data = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = data.split('\n').filter(line => line.trim() !== '');
+  if (lines.length === 0) {
+    console.log('Number of students: 0');
+    return;
+  }
+
+  const header = lines[0].split(',');
+
+  const studentsByField = {};
+
+  for (let i = 1; i < lines.length; i++) {
+    const row = lines[i].split(',');
+    if (row.length < 4) continue;
+    const firstName = row[0].trim();
+    const field = row[3].trim();
+    if (!studentsByField[field]) studentsByField[field] = [];
+    studentsByField[field].push(firstName);
+  }
+
+  const total = Object.values(studentsByField).reduce((acc, arr) => acc + arr.length, 0);
+  console.log(`Number of students: ${total}`);
+
+  for (const field in studentsByField) {
+    const list = studentsByField[field].join(', ');
+    console.log(`Number of students in ${field}: ${studentsByField[field].length}. List: ${list}`);
+  }
+}
+
+module.exports = countStudents;
