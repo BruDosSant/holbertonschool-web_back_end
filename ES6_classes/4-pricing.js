0dc416a43e582433f496b3c921c31a607624c14f
BruDosSant <brudossant@gmail.com>
2025-09-16 19:58:42 -0300

    messis

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from "./3-currency";
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('el monto debe ser un numero');
+        }
+        this._amount = amount;
+
+        if (!(currency instanceof Currency)) {
+            throw new TypeError('la moneda debe ser una instancia de la clase Currency');
+        }
+        this._currency = currency;
+    }
+    get amount() {
+        return this._amount;
+    }
+    set amount(newAmount) {
+        if (typeof newAmount !== 'number') {
+            throw new TypeError('el monto debe ser un numero');
+        }
+        this._amount = newAmount;
+    }
+    get currency() {
+        return this._currency;
+    }
+    set currency(newCurrency) {
+        if (!(newCurrency instanceof Currency)) {
+            throw new TypeError('la moneda debe ser una instancia de la clase Currency');
+        }
+        this._currency = newCurrency;
+    }
+    displayFullPrice() {
+        return `(${this._amount} ${this._currency.name} ${this.currency.code})`;
+    }
+}
